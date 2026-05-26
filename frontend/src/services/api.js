3a3a77970eb379beeea04fb3512f25711cbdc8f3
Bhavesh Sattavan <bhaveshsattavan@gmail.com>
2026-05-26 22:02:20 +0530

    fix axios production base ur

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,25 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 // Auto attach token
 api.interceptors.request.use((config) => {
   const userInfo = localStorage.getItem("userInfo");
+
   if (userInfo) {
     const { token } = JSON.parse(userInfo);
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
   }
+
   return config;
 });
 
